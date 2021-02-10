4c65e79e5e6e432dc9e65a4747eacc0551e895ff
JonasSvedmyr <jonas@svedmyr.nu>
2021-02-10 09:52:45 +0100

    Fixed Foreign key

--- SchemaMockUp/Models/AppointmentParticipant.cs
+++ SchemaMockUp/Models/AppointmentParticipant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SchemaMockUp.Models
@@ -9,8 +10,10 @@ namespace SchemaMockUp.Models
     {
         [Key]
         public int AppointmentParicipantID { get; set; }
-        public User User { get; set; } //Fk
-        public Appointment Appointment { get; set;} //Fk
+        [ForeignKey("User")]
+        public int UserID { get; set; } //Fk
+        [ForeignKey("Appointment")]
+        public int AppointmentID { get; set;} //Fk
         public string  Reminder { get; set; }
 
     }
